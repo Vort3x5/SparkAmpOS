81861ac144777e54fb563369932d769f8d3a7dc8
Vort3x5 <Faking.Games857@gmail.com>
2023-04-10 13:38:24 +0200

    Regset And Start Msg

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# References:
+- [OS-Dev Wiki] (https://wiki.osdev.org/Expanded_Main_Page)
+- [OS-Dev Guide] (http://independent-software.com/operating-system-development.html)
--- boot/boot.asm
+++ boot/boot.asm
@@ -4,99 +4,37 @@ org 0x7c00
 
 use16
 
-.start:	
-	call .enable_a20
-	jmp .loop
+.regset: ; Setup 16-bit segment registers and stack
+	push ax
 
-.loop:
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+
+	mov ss, ax
+	mov sp, 0x7c00
+
+	pop ax
+
+.start_msg db 'Initializing!', 0
+
+.start:	
+	call .print_startup
 	jmp $
 
-.enable_a20:
-	call .check_a20
-	call .a20_bios_enable
-	call .check_a20
-	call .a20_kb_enable
-	call .check_a20
-	call .a20_fast_enable
-	call .check_a20
-	ret
-
-.check_a20:
-	call .check_a20_proc
-	popf
-	cmp ax, 1
-	je .start
-	ret
-
-.check_a20_proc:
-	pushf
-	push ds
-	push es
-	push di
+.print_startup:
 	push si
-
-	cli
-	xor ax, ax
-	mov es, ax
-	not ax
-	mov ds, ax
-	mov di, 0x0500
-	mov si, 0x0510
-	mov al, byte [es:di]
-	push ax
-	mov al, byte [ds:si]
-	push ax
-	mov byte [es:di], 0x00
-	mov byte [ds:si], 0xFF
-	cmp byte [es:di], 0xFF
-	pop ax
-	mov byte [ds:si], al
-	pop ax
-	mov byte [es:di], al
-	mov ax, 0
-
-	pop si
-	pop di
-	pop es
-	pop ds
-	je .enable_a20
-	mov ax, 1
+	mov si, .start_msg
+	mov ah, 0x0e
+	call .print
 	ret
 
-.a20_enabled:
-	xor ax, ax
-	xor bx, bx
-	jmp .start
-
-.a20_bios_enable:
-	mov ax, 0x2401
-	int 0x15
+.print:
+	lodsb
+	int 0x10
+	cmp al, 0
+	jne .print
 	ret
 
-.a20_kb_enable:
-	cli
-	mov al, 0xd1
-	out 0x64, al
-	call .delay
-	mov al, 0xdf
-	in al, 0x60
-	sti
-	ret
-
-.a20_fast_enable:
-	in al, 0x92
-	or al, 2
-	out 0x92, al
-	ret
-
-.delay:
-	push cx
-	mov cx, 0xfffe
-	.delay_loop:
-		loop .delay_loop
-	pop cx
-	ret
-
-
-times 510 - ($ - .start) db 0
+times 510 - ($ - .regset) db 0
 dw 0xaa55
