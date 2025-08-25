76d1d3d975929895a7624e69e528c0a6871fb561
vortex <Faking.Games857@gmail.com>
2025-08-25 15:09:19 +0000

    The sound is finally here!!!!!!!!!!

--- drivers/ac97.c
+++ drivers/ac97.c
@@ -34,23 +34,23 @@ void PCIAC97Found(u32 bus, u32 dev, u32 function)
 
 void AC97Init()
 {
-	MMOutL(nabm_base + BUS_REG_GCTLR, 0x2);
-	MMOutW(nam_base + MIXER_REG_RESET, 0x1);
+	OutL(nabm_base + BUS_REG_GCTLR, 0x2);
+	OutW(nam_base + MIXER_REG_RESET, 0x1);
 
 	channel_capabilities = 
-		(MMInL(nabm_base + BUS_REG_GSTS) >> 20) & 0x03;
+		(InL(nabm_base + BUS_REG_GSTS) >> 20) & 0x03;
 	Print("Channel capabilities bits: ", WHITE);
 	PrintNum(channel_capabilities, LIGHT_CYAN);
 	PutC('\n', WHITE);
 
 	sample_capabilities = 
-		(MMInL(nabm_base + BUS_REG_GSTS) >> 22) & 0x03;
+		(InL(nabm_base + BUS_REG_GSTS) >> 22) & 0x03;
 	Print("Sample capabilities bits: ", WHITE);
 	PrintNum(sample_capabilities, LIGHT_CYAN);
 	PutC('\n', WHITE);
 
-	ext_capabilities = (MMInW(nam_base + MIXER_EXT_CAPABILITIES) & 0x08)
-		&& (MMInW(nam_base + MIXER_AUX_OUT_VOL) == 0x8000);
+	ext_capabilities = (InW(nam_base + MIXER_EXT_CAPABILITIES) & 0x08)
+		&& (InW(nam_base + MIXER_AUX_OUT_VOL) == 0x8000);
 	Print("Headphone output boolean value: ", WHITE);
 	PrintNum(ext_capabilities, LIGHT_CYAN);
 	PutC('\n', WHITE);
@@ -62,29 +62,28 @@ void AC97Init()
 	PutC('\n', WHITE);
 
 	// Mute at init
-	MMOutW(nam_base + MIXER_PCM_OUT_VOL, 0);
-	MMOutW(nam_base + MIXER_MASTER_OUT_VOL, 0);
+	OutW(nam_base + MIXER_PCM_OUT_VOL, 0);
+	OutW(nam_base + MIXER_MASTER_OUT_VOL, 0);
 }
 
 void AC97Play()
 {
-	MMOutW(nam_base + MIXER_PCM_OUT_VOL, 0x808);
-	MMOutW(nam_base + MIXER_MASTER_OUT_VOL, 0x808);
+	OutW(nam_base + MIXER_PCM_OUT_VOL, 0x808);
+	OutW(nam_base + MIXER_MASTER_OUT_VOL, 0x808);
 
 	FillBDL();
 
-	MMOutB(nabm_base + BUS_REG_RESET, 0x02);
-	while((MMInB(nabm_base + BUS_REG_RESET) & 0x2) == 0x2)
+	OutB(nabm_base + BUS_REG_RESET, 0x02);
+	while((InB(nabm_base + BUS_REG_RESET) & 0x2) == 0x2)
 		asm("nop");
-BRK();
 
-	MMOutL(nabm_base + BUS_PCM_OUT_BOX + BUS_ADDR_OF_BDL, (u32)bdl_ptr);
-	MMOutB(nabm_base + BUS_PCM_OUT_BOX + BUS_NUM_OF_BD_ENTRIES, 1);
+	OutL(nabm_base + BUS_PCM_OUT_BOX + BUS_ADDR_OF_BDL, (u32)bdl_ptr);
+	OutB(nabm_base + BUS_PCM_OUT_BOX + BUS_NUM_OF_BD_ENTRIES, 1);
 
-	MMOutB(nabm_base + BUS_REG_RESET, 0x01);
+	OutB(nabm_base + BUS_REG_RESET, 0x01);
 
 	// Start the PCM output stream
-	MMOutW(nam_base + BUS_PCM_OUT_BOX + BUS_TRANSFER_CTRL, 0x15);
+	OutW(nam_base + BUS_PCM_OUT_BOX + BUS_TRANSFER_CTRL, 0x15);
 }
 
 void FillBDL()
